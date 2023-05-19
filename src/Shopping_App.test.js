9afbdfa7a1b6bccfe42324c592634f24dc11f731
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-19 21:27:29 +0530

    linting changes

--- src/Shopping_App.test.js
+++ src/Shopping_App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import ShoppingApp from './Shopping_App';
+import { render, screen } from "@testing-library/react";
+import ShoppingApp from "./Shopping_App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<ShoppingApp />);
   const linkElement = screen.getByText(/shopping app/i);
   expect(linkElement).toBeInTheDocument();
