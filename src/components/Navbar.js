5b7a86e76de7bf39146d7fb95e49f31b40fc03fd
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-21 16:25:15 +0530

    added onClick event on the home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ onHomePageLinkClickedHandler }) => {
     return (
         <nav>
             <ul className="navlinks">
-                <li><Link to="/" style={{ textDecoration: "none" }}>Home</Link></li>
+                <li><Link to="/" onClick={onHomePageLinkClickedHandler} style={{ textDecoration: "none" }}>Home</Link></li>
                 <li><Link to="/products" style={{ textDecoration: "none" }}>Products</Link></li>
                 <li><Link to="/cart" style={{ textDecoration: "none" }}>Cart</Link></li>
             </ul>
