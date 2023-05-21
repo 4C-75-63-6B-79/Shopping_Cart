3074ecc5cab12e9107aae13e5489bd4f048a38d9
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-21 14:22:17 +0530

    thing get the products data
    like gets the data from the product of home page and the data for all the products and then sends the home page product data as a prop to it.

--- src/Shopping_App.js
+++ src/Shopping_App.js
@@ -1,6 +1,6 @@
 import "./Shopping_App.css";
 
-import React from "react";
+import React , { useState } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
@@ -8,7 +8,24 @@ import Products from "./pages/Products";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { getAllProductsData, getProductData } from "./modules/Get_Data";
+import { getRandomNumberInRange } from "./modules/helper_functions";
+
 function ShoppingApp() {
+
+  const [homePageProductData, setHomePageProductData] = useState(initalInitOfHomePageProductData);
+  const [allProductsData, setAllProductsData] = useState(initProductsData);
+
+  function initalInitOfHomePageProductData() {
+    getProductData(getRandomNumberInRange()).then((data) => setHomePageProductData(data));
+    return "";
+  }
+
+  function initProductsData() {
+    getAllProductsData().then((data) => setAllProductsData(data));
+    return "";
+  }
+
   return (
     <BrowserRouter>
       <div className="shopping-app">
@@ -18,7 +35,7 @@ function ShoppingApp() {
         <Navbar />
       </div>
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" exact element={<Home productData ={homePageProductData}/>} />
         <Route path="/products" element={<Products />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
