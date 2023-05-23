907404885139c00d03ad3ce1493918fce3d3e753
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-23 21:49:55 +0530

    removed the product description and added the button add to card and a function to handle the click event

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default function ProductCard({ productInfo }) {
+
+    function addToCardClicked(event) {
+        console.log(event);
+    }
+
     return (
         <>
             <div className="productCard">
                 <h2>{productInfo.title}</h2>
                 <h3>{productInfo.price}</h3>
-                <p>{productInfo.description}</p>
+                <button type="button" onClick={addToCardClicked}>add to cart</button>
             </div>
         </>
     );
