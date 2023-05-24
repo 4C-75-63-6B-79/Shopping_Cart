7acfd67ca7fe40e1543b2873b1dea426145657a3
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-24 21:51:45 +0530

    add to cart click handler is being passed a prop to this function and it passed to the product card

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,14 +2,15 @@ import React from "react";
 
 import ProductCard from "../components/ProductCard";
 
-const Products = ({ productsData }) => {
+const Products = ({ productsData, addToCartButtonClickHandler }) => {
+
     return (
         <>
             <h2>Products</h2>
             <div id="productGrid">
                 {
                     Array.from(productsData).map((product) => {
-                        return <ProductCard key={`${product.title}${product.id}`} productInfo={product}/>;
+                        return <ProductCard key={`${product.title}${product.id}`} productInfo={product} addToCartButtonClickHandler={addToCartButtonClickHandler}/>;
                     })
                 }
             </div>
