b258c1dbd65e78285a7c2004d7c9aea0f8100756
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-05-25 21:16:29 +0530

    add the number of items in cart to the link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navbar = ({ onHomePageLinkClickedHandler }) => {
+const Navbar = ({ onHomePageLinkClickedHandler, numberOfProductsInCart }) => {
     return (
         <nav>
             <ul className="navlinks">
                 <li><Link to="/" onClick={onHomePageLinkClickedHandler} style={{ textDecoration: "none" }}>Home</Link></li>
                 <li><Link to="/products" style={{ textDecoration: "none" }}>Products</Link></li>
-                <li><Link to="/cart" style={{ textDecoration: "none" }}>Cart</Link></li>
+                <li><Link to="/cart" style={{ textDecoration: "none" }}>Cart {numberOfProductsInCart}</Link></li>
             </ul>
         </nav>
     );
