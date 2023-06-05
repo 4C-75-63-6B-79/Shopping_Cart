6644f20e22d68d2a87d061fabad0df052486e4e6
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-05 21:25:06 +0530

    added a option for the quantities of the products

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,6 +11,12 @@ export default function ProductCard({ productInfo, addToCartButtonClickHandler }
             <div className="productCard">
                 <h2>{productInfo.title}</h2>
                 <h3>{productInfo.price}</h3>
+                <label htmlFor="quantity-select">Quantity</label>
+                <select name="quantity" id="quantity-select">
+                    {
+                        Array.from({ length: 10 }, () => null).map(( element, index ) => <option key={"quantityOptions"+index} value={index + 1}>{index + 1}</option>)                        
+                    }
+                </select>
                 <button type="button" onClick={addToCartClicked}>add to cart</button>
             </div>
         </>
