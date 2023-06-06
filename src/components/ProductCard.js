9b8922dcdf06846fb43ec4fa7a57f2b47b84c4d2
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-06 20:39:35 +0530

    added a state of quantity which changes on change of the select element and not the addToCart button sends a object which has quantity property

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 export default function ProductCard({ productInfo, addToCartButtonClickHandler }) {
 
+    const[quantity, setQuantity] = useState(1);
+
     function addToCartClicked(event) {
-        addToCartButtonClickHandler(productInfo);
+        addToCartButtonClickHandler({ ...productInfo, quantity: quantity });
+    }
+
+    function onChangeHandlerForSelect(event) {
+        setQuantity(() => event.target.value);
     }
 
     return (
@@ -12,7 +19,7 @@ export default function ProductCard({ productInfo, addToCartButtonClickHandler }
                 <h2>{productInfo.title}</h2>
                 <h3>{productInfo.price}</h3>
                 <label htmlFor="quantity-select">Quantity</label>
-                <select name="quantity" id="quantity-select">
+                <select name="quantity" id="quantity-select" onChange={onChangeHandlerForSelect}>
                     {
                         Array.from({ length: 10 }, () => null).map(( element, index ) => <option key={"quantityOptions"+index} value={index + 1}>{index + 1}</option>)                        
                     }
