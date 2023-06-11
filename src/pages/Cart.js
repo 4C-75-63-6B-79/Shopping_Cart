6e037e1435f86a2e6f025875db5e928110bf6811
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-11 19:14:32 +0530

    modified the logic to filter the incart items to render them

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ProductInCartCard from "../components/ProductInCartCard";
 
-const Cart = ({ productsInCart, deleteFromCartButtonClickHandler }) => {
+const Cart = ({ allProductsData, deleteFromCartButtonClickHandler }) => {
     return (
         <>
             <h2>Cart</h2>
             <div id="cartProductGrid">
                 {
-                    Object.keys(productsInCart).length === 0 ? <h3>Your cart is empty</h3> : Object.entries(productsInCart).map(([cartItemTitle, cartItemInfo]) => <ProductInCartCard key={`${cartItemTitle}${cartItemInfo.id}`} deleteFromCartButtonClickHandler={deleteFromCartButtonClickHandler} productInfo={cartItemInfo}/>)
+                    Object.entries(allProductsData).some(([index, productInfo]) => productInfo["inCart"]) ? Object.entries(allProductsData).filter(([index, productInfo]) => productInfo["inCart"]).map(([index, cartItemInfo]) => <ProductInCartCard key={`${cartItemInfo.title}${cartItemInfo.id}`} deleteFromCartButtonClickHandler={deleteFromCartButtonClickHandler} productInfo={cartItemInfo}/>) : <h3>Your cart is empty</h3>
                 }
             </div>
         </>
