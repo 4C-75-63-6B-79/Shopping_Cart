9563750948b1b68d574ec071ddef07d309db509e
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-11 10:19:43 +0530

    cart page display the title and quantity of the items in the cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,15 @@
 import React from "react";
+import ProductInCartCard from "../components/ProductInCartCard";
 
-const Cart = () => {
+const Cart = ({ productsInCart }) => {
     return (
         <>
             <h2>Cart</h2>
+            <div id="cartProductGrid">
+                {
+                    Object.keys(productsInCart).length === 0 ? <h3>Your cart is empty</h3> : Object.entries(productsInCart).map(([cartItemTitle, cartItemInfo]) => <ProductInCartCard key={`${cartItemTitle}${cartItemInfo.id}`} productInfo={cartItemInfo}/>)
+                }
+            </div>
         </>
     );
 };
