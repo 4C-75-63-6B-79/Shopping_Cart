981aa05526526ae7431b5c86ea8191fe10c25f13
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-11 17:28:02 +0530

    added the value to the select tag

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -19,9 +19,9 @@ export default function ProductCard({ productInfo, addToCartButtonClickHandler }
                 <h2>{productInfo.title}</h2>
                 <h3>{productInfo.price}</h3>
                 <label htmlFor="quantity-select">Quantity</label>
-                <select name="quantity" id="quantity-select" onChange={onChangeHandlerForSelect}>
+                <select name="quantity" id="quantity-select" onChange={onChangeHandlerForSelect} value={quantity}>
                     {
-                        Array.from({ length: 10 }, () => null).map(( element, index ) => <option key={"quantityOptions"+index} value={index + 1}>{index + 1}</option>)                        
+                        Array.from({ length: 10 }, () => null).map(( element, index ) => <option key={"quantityOptions"+index} value={index + 1} >{index + 1}</option>)                        
                     }
                 </select>
                 <button type="button" onClick={addToCartClicked}>add to cart</button>
