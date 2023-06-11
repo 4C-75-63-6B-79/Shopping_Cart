ab7e98ed6372cc8582afa2364e64699ef5d5ac48
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-06-11 16:51:45 +0530

    passing the delete from cart button click handler as prop to the products in cart card component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ProductInCartCard from "../components/ProductInCartCard";
 
-const Cart = ({ productsInCart }) => {
+const Cart = ({ productsInCart, deleteFromCartButtonClickHandler }) => {
     return (
         <>
             <h2>Cart</h2>
             <div id="cartProductGrid">
                 {
-                    Object.keys(productsInCart).length === 0 ? <h3>Your cart is empty</h3> : Object.entries(productsInCart).map(([cartItemTitle, cartItemInfo]) => <ProductInCartCard key={`${cartItemTitle}${cartItemInfo.id}`} productInfo={cartItemInfo}/>)
+                    Object.keys(productsInCart).length === 0 ? <h3>Your cart is empty</h3> : Object.entries(productsInCart).map(([cartItemTitle, cartItemInfo]) => <ProductInCartCard key={`${cartItemTitle}${cartItemInfo.id}`} deleteFromCartButtonClickHandler={deleteFromCartButtonClickHandler} productInfo={cartItemInfo}/>)
                 }
             </div>
         </>
