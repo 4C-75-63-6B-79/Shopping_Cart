ebf9cd88c9533771636cec86b5e37bad5803c1eb
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-07-03 21:50:24 +0530

    added describe to the home page test

--- src/pages/Home.test.js
+++ src/pages/Home.test.js
@@ -2,30 +2,33 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "./Home";
 
-test("renders home with product title and with product img", () => {
+describe("testing home page", () => {
     const mockProductData = { "image": "#", "title": "This is a test product" };
-    
-    render(<Home productData={mockProductData}/>);
-    
-    const homeH2 = screen.getByRole("heading", { "level":2 });
-    const productTitleH3 = screen.getByRole("heading", { "level": 3 });
-    const productImg = screen.getByRole("img");
-    
-    expect(homeH2.textContent).toMatch("Home");
-    expect(productTitleH3.textContent).toMatch("This is a test product");
-    expect(productImg).toBeInTheDocument();
-    expect(productImg).toHaveAttribute("src", "#");
-    expect(productImg).toHaveAttribute("alt", "This is a test product");
-});
-
-test("renders home with error if there is error in getting product data", () => {
     const mockProductDataError = { error: "Something went wrong" };
+
+    test("renders home with product title and with product img", () => {
+        render(<Home productData={mockProductData}/>);
+        
+        const homeH2 = screen.getByRole("heading", { "level":2 });
+        const productTitleH3 = screen.getByRole("heading", { "level": 3 });
+        const productImg = screen.getByRole("img");
+        
+        expect(homeH2.textContent).toMatch("Home");
+        expect(productTitleH3.textContent).toMatch("This is a test product");
+        expect(productImg).toBeInTheDocument();
+        expect(productImg).toHaveAttribute("src", "#");
+        expect(productImg).toHaveAttribute("alt", "This is a test product");
+    });
     
-    render(<Home productData={mockProductDataError}/>);
+    test("renders home with error if there is error in getting product data", () => {
+        render(<Home productData={mockProductDataError}/>);
+    
+        const homeh3 = screen.getByRole("heading", { "level": 3 });
+        const productImg = screen.queryByRole("img");
+    
+        expect(homeh3.textContent).toMatch("Something went wrong");
+        expect(productImg).toBeNull();
+    });
 
-    const homeh3 = screen.getByRole("heading", { "level": 3 });
-    const productImg = screen.queryByRole("img");
-
-    expect(homeh3.textContent).toMatch("Something went wrong");
-    expect(productImg).toBeNull();
 });
+
