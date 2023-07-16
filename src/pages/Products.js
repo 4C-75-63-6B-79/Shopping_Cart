3e132c55948a329483df057d7a18c6423de80604
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-07-16 15:48:02 +0530

    add select list to sort the products by category

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -6,10 +6,20 @@ const Products = ({ allProductsData, addToCartButtonClickHandler }) => {
 
     return (
         <>
-            <h2>Products</h2>
+            <div id="productsPageNavbar">
+                <h2>Products</h2>
+                <label htmlFor="category-select">Sort By Category: </label>
+                <select name="category" id="category-select" default="all">
+                    <option value="all">All</option>
+                    <option value="mensClothing">Men's Clothing</option>
+                    <option value="womensClothing">Women's Clothing</option>
+                    <option value="jewellery">Jewellery</option>
+                    <option value="Electronics">Electronics</option>
+                </select>
+            </div>
             <div id="productGrid">
                 {
-                    allProductsData.error ? <h3>{allProductsData.error}</h3> :
+                    allProductsData["loading"] === true ? "Loading..." : allProductsData.error ? <h3>{allProductsData.error}</h3> :
                     Object.entries(allProductsData).map(([index, product]) => {
                         return <ProductCard key={`${product.title}${product.id}`} productInfo={product} addToCartButtonClickHandler={addToCartButtonClickHandler}/>;
                     })
